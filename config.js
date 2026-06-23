ec931cbca39adb66a3631e89e40e0d6ab9ca672d
Aymane3369 <168319843+Aymane3369@users.noreply.github.com>
2026-06-23 13:06:39 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    BIN_ID: '6a3a63d2da38895dfef0dfa9',
+    API_KEY: '$2a$10$yYujh3uZTe5eYF5tEXW9qO5nU8rw735V9ZQZ3I0b80cPvxCSowHBu',
+    BASE_URL: 'https://api.jsonbin.io/v3/b/'
+};
