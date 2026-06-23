7c878bd02072b83fa0bd1ef78c5120cf860465b9
Aymane3369 <168319843+Aymane3369@users.noreply.github.com>
2026-06-23 14:01:49 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    BIN_ID: '6a3a6cf0da38895dfef10b2a',
-    API_KEY: '$2a$10$yYujh3uZTe5eYF5tEXW9qO5nU8rw735V9ZQZ3I0b80cPvxCSowHBu',
-    BASE_URL: 'https://api.jsonbin.io/v3/b/'
-};
